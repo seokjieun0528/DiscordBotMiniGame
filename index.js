eac0468e0be7ca5632f66ffc92fd6fc9eaf02980
seokjieun0528 <s2307@e-mirim.hs.kr>
2025-06-23 00:51:23 +0900

    디스코드 봇 만들기 초기 설정

---
+++ index.js
@@ -0,0 +1,24 @@
+const { Client, Events, GatewayIntentBits } = require("discord.js");
+const { token } = require("./config.json");
+
+// 클라이언트 객체 생성
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+client.once(Events.ClientReady, (readyClient) => {
+  console.log(`로그인 준비 ${readyClient.user.tag}`);
+});
+
+//!hi라는 메시지를 받으면 "안녕!"이라고 대답하기
+client.on("messageCreate", (message) => {
+  if (message.content == "!hi") {
+    message.reply("안녕!");
+  }
+});
+
+client.login(token);
